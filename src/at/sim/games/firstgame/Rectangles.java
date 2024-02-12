144f31cde57cba158ef6a572a64c1e31a9d4e42a
Simon <simon.metzler@student.htldornbirn.at>
2024-02-12 14:39:49 +0100

    Objects

--- src/at/sim/games/firstgame/Rectangles.java
+++ src/at/sim/games/firstgame/Rectangles.java
@@ -5,10 +5,10 @@ import org.newdawn.slick.tests.AnimationTest;
 
 public class Rectangles extends BasicGame {
 
-    private int rect_x;
-    private int rect_y;
-    private int rect_vel_x;
-    private int rect_vel_y;
+    private double rect_x;
+    private double rect_y;
+    private double rect_vel_x;
+    private double rect_vel_y;
     private int rect_width;
     private int rect_height;
     private int window_width;
@@ -20,19 +20,19 @@ public class Rectangles extends BasicGame {
 
     @Override
     public void init(GameContainer gameContainer) throws SlickException {
-        this.rect_x = 200;
-        this.rect_y = 200;
-        this.rect_vel_x = 1;
-        this.rect_vel_y = 1;
+        this.rect_x = 0;
+        this.rect_y = 0;
+        this.rect_vel_x = 0.4f;
+        this.rect_vel_y = 0.4f;
         this.rect_width = 100;
         this.rect_height = 50;
         this.window_width = 1000;
-        this.window_height = 800;
+        this.window_height = 1000;
 
     }
 
     @Override
-    public void update(GameContainer gameContainer, int i) throws SlickException {
+    public void update(GameContainer gameContainer, int delta) throws SlickException {
         if (this.rect_x > this.window_width-this.rect_width || this.rect_x < 0){
             this.rect_vel_x = -this.rect_vel_x;
         }
@@ -40,21 +40,22 @@ public class Rectangles extends BasicGame {
             this.rect_vel_y = -this.rect_vel_y;
         }
 
-        this.rect_x += this.rect_vel_x;
-        this.rect_y += this.rect_vel_y;
+        this.rect_x += (delta*this.rect_vel_x);
+        this.rect_y += (delta*this.rect_vel_y);
 
     }
 
     @Override
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
-        graphics.drawRect(this.rect_x, this.rect_y, this.rect_width, rect_height);
+        graphics.drawRect((int)this.rect_x, (int)this.rect_y, this.rect_width, rect_height);
         graphics.drawString("Hello World!", 300, 300);
+
     }
 
     public static void main(String[] argv) {
         try {
             AppGameContainer container = new AppGameContainer(new Rectangles("Rectangles"));
-            container.setDisplayMode(1000,800,false);
+            container.setDisplayMode(1000,1000,false);
             container.start();
         } catch (SlickException e) {
             e.printStackTrace();
