ec36a423dde59233e8e51183c6a402f4545ae646
Simon <simon.metzler@student.htldornbirn.at>
2024-04-24 22:21:34 +0200

    finished minesweeper

--- src/at/sim/games/minesweeper/Board.java
+++ src/at/sim/games/minesweeper/Board.java
@@ -20,7 +20,9 @@ public class Board implements Actor {
 
     private String[] image_names;
     private boolean game_over;
-    private boolean genereated;
+    private boolean game_finished;
+    private boolean generated;
+
 
     public Board(int height, int width, int tile_size) throws SlickException {
         this.height = height;
@@ -32,7 +34,7 @@ public class Board implements Actor {
 
         this.images = new ArrayList<>();
 
-        this.image_names = new String[]{"unknown", "mine", "known", "1", "2", "3", "4", "5", "6", "7", "8", "flag"};
+        this.image_names = new String[]{"known", "1", "2", "3", "4", "5", "6", "7", "8", "unknown", "mine", "flag"};
 
 
         Image temp;
@@ -41,9 +43,16 @@ public class Board implements Actor {
             this.images.add(temp.getScaledCopy(this.tile_size, this.tile_size));
         }
 
+        temp = new Image("src/at/sim/games/minesweeper/img/won.png");
+        this.images.add(temp.getScaledCopy(500, 300));
+
+        temp = new Image("src/at/sim/games/minesweeper/img/lost.png");
+        this.images.add(temp.getScaledCopy(500, 300));
+
+
         this.game_over = false;
 
-        this.genereated = false;
+        this.generated = false;
 
 
     }
@@ -59,6 +68,7 @@ public class Board implements Actor {
     }
 
     public void generateBoard(int startX, int startY, int maxMines) {
+
         int minesCount = 0;
         int randomX, randomY;
         while (minesCount < maxMines) {
@@ -109,7 +119,7 @@ public class Board implements Actor {
         for (int y = 0; y < this.height; y++) {
             for (int x = 0; x < this.width; x++) {
 
-                if (!this.board[x][y].isKnown()) {
+                if (!this.board[x][y].isKnown() && !this.board[x][y].isFlagged()) {
                     game_finished = false;
                 }
 
@@ -118,7 +128,7 @@ public class Board implements Actor {
                     int abs_neighbour_pos_y = y + relative_neighbour_position[1];
 
                     if (abs_neighbour_pos_x >= 0 && abs_neighbour_pos_x < this.width && abs_neighbour_pos_y >= 0 && abs_neighbour_pos_y < this.height) {
-                        if (this.board[abs_neighbour_pos_x][abs_neighbour_pos_y].isKnown() || this.board[abs_neighbour_pos_x][abs_neighbour_pos_y].getMines() == 0) {
+                        if (this.board[abs_neighbour_pos_x][abs_neighbour_pos_y].isKnown() && this.board[abs_neighbour_pos_x][abs_neighbour_pos_y].getMines() == 0) {
                             this.board[x][y].setKnown(true);
                         }
 
@@ -128,6 +138,11 @@ public class Board implements Actor {
             }
 
         }
+
+        if (game_finished) {
+            this.game_finished = true;
+        }
+
     }
 
 
@@ -138,13 +153,16 @@ public class Board implements Actor {
             for (int x = 0; x < this.width; x++) {
                 tile = this.board[x][y];
                 if (!tile.isKnown()) {
-                    if (tile.isFlagged()) {
-                        this.images.get(9).draw(x * this.tile_size, y * this.tile_size);
+
+                    if (this.game_over && this.board[x][y].isMine()) {
+                        this.images.get(10).draw(x * this.tile_size, y * this.tile_size);
+                    } else if (tile.isFlagged()) {
+                        this.images.get(11).draw(x * this.tile_size, y * this.tile_size);
                     } else {
-                        this.images.get(0).draw(x * this.tile_size, y * this.tile_size);
+                        this.images.get(9).draw(x * this.tile_size, y * this.tile_size);
                     }
-                } else if (tile.isKnown()) {
-                    this.images.get(tile.getMines() + 2).draw(x * this.tile_size, y * this.tile_size);
+                } else {
+                    this.images.get(tile.getMines()).draw(x * this.tile_size, y * this.tile_size);
                 }
 
 
@@ -152,43 +170,71 @@ public class Board implements Actor {
 
         }
 
+        if (this.game_over) {
+            this.images.get(13).draw(250, 350);
+        } else if (this.game_finished) {
+            this.images.get(12).draw(250, 350);
+        }
+
+
     }
 
     @Override
     public void update(GameContainer gameContainer, int delta) {
         Input input = gameContainer.getInput();
-        if (input.isMousePressed(0)) {
-            int x = input.getMouseX() / this.tile_size;
-            int y = input.getMouseY() / this.tile_size;
 
-            if (this.genereated == false) {
-                generateBoard(x, y, 20);
-                this.genereated = true;
+        if (!this.game_over && !this.game_finished) {
 
-            } else if (this.board[x][y].isMine()) {
-                this.game_over = true;
+            if (input.isMousePressed(0)) {
+                int x = input.getMouseX() / this.tile_size;
+                int y = input.getMouseY() / this.tile_size;
 
-            } else if (this.board[x][y].isKnown()) {
-                this.board[x][y].setKnown(true);
-            }
+                if (this.generated == false) {
+                    generateBoard(x, y, 15);
+                    this.generated = true;
 
-        }
+                } else if (this.board[x][y].isMine()) {
+                    this.game_over = true;
 
+                } else if (!this.board[x][y].isKnown() && !this.board[x][y].isMine()) {
+                    this.board[x][y].setKnown(true);
+                }
 
-        if (input.isMousePressed(1)) {
-            int x = input.getMouseX() / this.tile_size;
-            int y = input.getMouseY() / this.tile_size;
-
-            Tile tile = this.board[x][y];
-
-            if (!tile.isKnown()) {
-                tile.setFlagged(!tile.isFlagged());
             }
 
 
-        }
+            if (input.isMousePressed(1)) {
+                int x = input.getMouseX() / this.tile_size;
+                int y = input.getMouseY() / this.tile_size;
 
-        updateBoard();
+                Tile tile = this.board[x][y];
+
+                if (!tile.isKnown()) {
+                    tile.setFlagged(!tile.isFlagged());
+                }
+
+
+            }
+
+            updateBoard();
+        } else {
+
+            if (input.isMousePressed(0)) {
+                int x = input.getMouseX();
+                int y = input.getMouseY();
+
+                if (x >= 250 && x <= 750 && y >= 250 && y <= 750) {
+                    this.game_finished = false;
+                    this.game_over = false;
+                    this.generated = false;
+                    resetBoard();
+
+                }
+
+
+            }
+
+        }
 
     }
 }
